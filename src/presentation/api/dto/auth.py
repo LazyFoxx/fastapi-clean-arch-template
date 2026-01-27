8b33d321e3e4e9f6376460e1bb9e5e9101e8286a
LazyFoxx <chernomor987654321@gmail.com>
2026-01-27 17:36:30 +0300

    шаблон фастапи собранный из сервиса авторизации

---
+++ src/presentation/api/dto/auth.py
@@ -0,0 +1,86 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class RegisterRequest(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+
+
+class LoginRequest(RegisterRequest):
+    email: EmailStr
+    password: str = Field(..., min_length=8)
+
+
+class MessageResponse(BaseModel):
+    message: str = Field(..., description="Информационное сообщение для клиента")
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {"message": "Код верификации успешно отправлен!"}
+        }
+    }
+
+
+class EmailVerificationRequest(BaseModel):
+    email: EmailStr
+    code: str
+
+
+class ResendEmailVerificationRequest(BaseModel):
+    email: EmailStr
+
+
+class RegistrationCompletedResponse(BaseModel):
+    message: str = "Регистрация прошла успешно!"
+    access_token: str
+    token_type: str = "bearer"
+    expires_in: int  # время жизни access-токена в секундах
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": "Регистрация прошла успешно!",
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                "token_type": "bearer",
+                "expires_in": 1800,
+            }
+        }
+    }
+
+
+class LoginResponse(BaseModel):
+    message: str = "Успешная авторизация!"
+    access_token: str
+    token_type: str = "bearer"
+    expires_in: int  # время жизни access-токена в секундах
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": "Успешная авторизация!",
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                "token_type": "bearer",
+                "expires_in": 1800,
+            }
+        }
+    }
+
+
+class TokenAccessResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    expires_in: int  # время жизни access-токена в секундах
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                "token_type": "bearer",
+                "expires_in": 1800,
+            }
+        }
+    }
+
+
+class NewPasswordRequest(BaseModel):
+    password: str = Field(..., min_length=8)
