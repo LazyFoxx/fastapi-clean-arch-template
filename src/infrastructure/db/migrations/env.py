74c053d4e52be38b899b1f21c4ae28a00744cdd3
dmitry <dmitry@example.com>
2026-01-31 17:49:37 +0300

    добавил миграции алембик

--- src/infrastructure/db/migrations/env.py
+++ src/infrastructure/db/migrations/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from src.infrastructure.db.base import Base
+import src.infrastructure.db.models
 from src.core.settings.database import settings
 
 # this is the Alembic Config object, which provides
